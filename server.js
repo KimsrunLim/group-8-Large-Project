371e406ba5120a6cdcd3e81c63d7a4bf7582debf
Frosty204 <austinfrost04@gmail.com>
2024-03-03 17:46:40 -0500

    Update server.js
    Fixed error

--- server.js
+++ server.js
@@ -9,8 +9,31 @@ app.use(bodyParser.json());
 const url = 'mongodb+srv://admin:COP4331@group8large.ndz51wr.mongodb.net/?retryWrites=true&w=majority&appName=group8large';
 const MongoClient = require("mongodb").MongoClient;
 const client = new MongoClient(url);
-client.connect(consol.log("mongodb connected"));
+client.connect(console.log("mongodb connected"));
 
+app.post('/api/users', async (req, res, next) => {
+    var error = '';
+
+    const { username, password } = req.body;
+
+    const db = client.db("group8large");
+
+    const results = await
+        db.collection('users').find({ Login : username, Password : password}).toArray();
+    
+    var id = -1;
+    var fn = '';
+    var ln = '';
+    
+    if (results.length > 0) {
+        id = results[0]._id;
+        fn = results[0].FirstName;
+        ln = results[0].LastName;
+    }
+
+    var ret = { id:id, firstName:fn, lastName:ln, error: '' };
+    res.status(200).json(ret);
+});
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
