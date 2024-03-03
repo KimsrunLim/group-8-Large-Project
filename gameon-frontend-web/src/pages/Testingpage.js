ed2c7c7a204dee14374a52dd8c57cdb35a916701
Yixin Zhao <st97334@gmail.com>
2024-03-03 16:13:52 -0500

    react environment fix

---
+++ gameon-frontend-web/src/pages/Testingpage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Test from '../components/Test';
+
+
+const Testingpage = () => {
+    return (
+        <div>
+            <Test />
+        </div>
+    );
+};
+
+export default Testingpage;
