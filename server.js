9dbb8fd4252259c408e0a7693ee0b4e122274c5a
Frosty204 <austinfrost04@gmail.com>
2024-03-03 17:18:21 -0500

    Update server.js
    Connected to databse

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+const url = 'mongodb+srv://admin:COP4331@group8large.ndz51wr.mongodb.net/?retryWrites=true&w=majority&appName=group8large';
+const MongoClient = require("mongodb").MongoClient;
+const client = new MongoClient(url);
+client.connect(consol.log("mongodb connected"));
+
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
