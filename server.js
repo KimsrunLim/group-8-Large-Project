b61cd0cf041ddb79cf443bc8f92d5c3a96f1e780
Yixin Zhao <st97334@gmail.com>
2024-03-09 01:19:19 -0500

    Port Update do to porting issues for MAC

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5001));
 
 app.use(cors());
 app.use(bodyParser.json());
