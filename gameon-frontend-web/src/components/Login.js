bcd1e11905190c3a178f4750a2221fb57331dd22
KimsrunLim <ksrunlim@gmail.com>
2024-04-03 00:50:16 -0400

    Login.js Corrected

--- gameon-frontend-web/src/components/Login.js
+++ gameon-frontend-web/src/components/Login.js
@@ -57,100 +57,71 @@ function Login() {
         }
     };
 
+    const handleGuest = () => {
+        window.location.href = "/home";
+        return;
+    };
+
     return (
         <>
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-md-6">
                         { /* Logo */}
-                        <div className="logo text-center mb-4 pb-4 pt-4">
-                            <a href="/home">
-                                <img src="/logo192.png" alt="GameOn Logo"
-                                    style={{ width: '10rem', height: '10rem', marginTop: '5%' }} />
-                            </a>
+                        <div className="logo p-4 text-center m-auto">
+                            <img src="/logo192.png" className='img-fluid' alt="GameOn Logo" style={{ maxWidth: "150px" }} />
                         </div>
                         <div className="login-container">
+
                             { /* Login Box */}
                             <div className="card">
+
                                 { /* Title */}
-                                {/* <h2 className="card-header text-center pb-4 pt-2">Log In</h2> */}
-                                <span className="card-header text-center pb-4 pt-2" style={{ fontSize: '2rem', fontWeight: 'bold', color: '#000' }}>
-                                    Log In
-                                </span>
+                                <h1 className="card-header p-3 text-center align-middle fw-bold text-black">Log In</h1>
                                 <div className="card-body">
+
                                     { /* Form */}
                                     <form onSubmit={doLogin}>
-                                        { /* validation functionality: 
-                                        * - valid -> no effects
-                                        * - invalid -> red text input highlight (replace bootstrap's blue) 
-                                        *              & red icon    */ }
 
                                         { /* Username */}
                                         <div className="form-group pt-4">
-                                            <label htmlFor="username" className="mb-1"
-                                                style={{ fontWeight: 600, color: '#444' }}>
-                                                Username
-                                            </label>
-                                            <div className="input-group mb-4">
-                                                <div className="input-group-prepend">
-                                                    <span className="input-group-text">
-                                                        <FontAwesomeIcon icon={faUser} />
-                                                    </span>
-                                                </div>
-                                                <input type="text" id="username" className="form-control"
-                                                    placeholder="Username" ref={(c) => username = c} />
+                                            <label className="pb-1 text-secondary fw-bold">Username</label>
+                                            <div className="input-group mb-3">
+                                                <span className="input-group-text"><FontAwesomeIcon icon={faUser} /></span>
+                                                <input type="text" id="username" className="form-control" placeholder="Username" ref={(c) => username = c} />
                                             </div>
                                         </div>
+
                                         { /* Password */}
-                                        <div className="form-group">
-                                            <label htmlFor="password" className="mb-1"
-                                                style={{ fontWeight: 600, color: '#444' }}>
-                                                Password
-                                            </label>
-                                            <div className="input-group mb-4">
-                                                <div className="input-group-prepend">
-                                                    <span className="input-group-text">
-                                                        <FontAwesomeIcon icon={faLock} />
-                                                    </span>
-                                                </div>
-                                                <input type="text" id="password" className="form-control"
-                                                    placeholder="Password" ref={(c) => password = c} />
+                                        <div className="form-group pt-1">
+                                            <label className="pb-1 text-secondary fw-bold">Password</label>
+                                            <div className="input-group mb-3">
+                                                <span className="input-group-text"><FontAwesomeIcon icon={faLock} /></span>
+                                                <input type="password" id="password" className="form-control" placeholder="Password" ref={(c) => password = c} />
                                             </div>
                                         </div>
                                         { /* Error Feedback */}
                                         { /* idea: red container highlight on error message... flash/until type */}
                                         {message &&
-                                            <div className="alert alert-danger mt-1 mb-2 pt-2 pb-2 text-center mx-auto"
-                                                style={{
-                                                    border: 'none', backgroundColor: '#fff',
-                                                    fontSize: '1rem', fontWeight: 'bold',
-                                                    color: '#7D0000', width: '70%'
-                                                }}>
+                                            <div className="text-danger mt-1 mb-2 pt-2 pb-2 text-center mx-auto fs-6 fw-bold">
                                                 {message}
                                             </div>}
+
                                         { /* Submit */}
-                                        <button type="submit" className="btn btn-primary w-100 mt-3 mb-1"
-                                            style={{
-                                                backgroundColor: '#000', borderColor: '#000',
-                                                fontSize: '1.2rem', fontWeight: 500, color: '#fff'
-                                            }}>
-                                            Submit
-                                        </button>
+                                        <button type="submit" className="btn bg-black w-100 mt-3 mb-1 fs-5 text-white fw-bold">Submit</button>
                                     </form>
-                                </div>
-                            </div>
-                            { /* Switch to Signup */}
-                            <a href="/signup" className="alert-link mb-4"
-                                style={{ display: 'block', textDecoration: 'none' }}>
-                                <div className="text-center">
-                                    <div className="alert alert-secondary" role="alert"
-                                        style={{ fontWeight: 600, color: '#444' }}>
-                                        New to GameOn? <a href="/signup"
-                                            className="alert-link mt-4 mb-4"
-                                            style={{ fontWeight: 'bold', color: '#000' }} >Sign Up</a>
+
+                                    <div className='d-flex justify-content-center align-items-center text-center p-2 fw-medium'>
+                                        New to GameOn?
+                                        <a href="/signup" className='link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover ps-3'>Sign Up</a>
                                     </div>
                                 </div>
-                            </a>
+                            </div>
+
+                            { /* Switch to Signup */}
+                            <div className='d-grid py-5'>
+                                <button onClick={handleGuest} class="btn border border-3 border-dark fw-bold" type="button" style={{maxHeight: "50px", height: "50px"}}>Continue as Guest</button>
+                            </div>
                         </div>
                     </div>
                 </div>
