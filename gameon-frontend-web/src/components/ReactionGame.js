cacb8ce94402818f9d9548d63160c9c0a2196ef6
Yixin Zhao <st97334@gmail.com>
2024-04-03 04:11:06 -0400

    clean up code and make the canvas color change during game

--- gameon-frontend-web/src/components/ReactionGame.js
+++ gameon-frontend-web/src/components/ReactionGame.js
@@ -1,5 +1,4 @@
 import { useEffect, useState, useRef } from "react";
-import $ from "jquery";
 
 function ReactionGame()
 {
@@ -9,10 +8,7 @@ function ReactionGame()
     const [game, setGame] = useState(false);
     const intervalRef = useRef(null);
     const canvasRef = useRef(null);
-    $("body").css("background-color", "white");
     
-  
-
     const randomTime = (max) => {
         let time = Math.floor(Math.random() * Math.floor(max) ) + 1;
         time *= 1000;
@@ -26,16 +22,15 @@ function ReactionGame()
             let data1 = new Date();
             let start = data1.getTime();
             setStartTime(start);
-            $("body").css("background-color", "green");
-            canvasRef.current.style.backgroundColor = 'rgb(78,197,78)';
+            canvasRef.current.style.backgroundColor = 'rgb(78,197,78)'; //green
             console.log("starttime:", start);
-            timer = setInterval(() => {
-                setPlayTime(playTime+1);
-            }, 1)
+            // timer = setInterval(() => {
+            //     setPlayTime(playTime+1);
+            // }, 1)
 
             canvasRef.current.addEventListener('click', function() {
                 let date2 = new Date().getTime();
-                console.log("endingtime: ",date2)
+                console.log("endingtime: ", date2);
                 setEndTime(date2 - start);
             })
         }, time)
@@ -44,7 +39,7 @@ function ReactionGame()
 
     
     const Start = () => {
-        $("body").css("background-color", "white");
+        canvasRef.current.style.backgroundColor = 'rgb(243,16,16)';
         console.log("game start");
         let changeColorTime = randomTime(8); //max time as 8 sec.
         setStartTime(0);
@@ -55,16 +50,12 @@ function ReactionGame()
 
     };
 
-    const calcTime = () => {
+    const endGame = () => {
         console.log("gameover");
-        let date2 = new Date();
-        let end = date2.getTime();
-        // setEndTime(end - startTime);
         setGame(false);
-        $("body").css("background-color", "red");
+        canvasRef.current.style.backgroundColor = 'rgb(249,236,61)';
         clearInterval(timer);
         setStartTime(0);
-        return end;
     };
     return(
         <div class="text-center">
@@ -73,11 +64,11 @@ function ReactionGame()
                 <p>Click Screen when color changed</p>
             </div>
             <div>
-                <canvas className="bg-black" ref={canvasRef} onClick={calcTime}>green?</canvas>
             </div>
             <button className="btn btn-primary" onClick={Start}>Start</button>
             <p>Time: </p>
             {endTime !== null && <p>{endTime} ms</p>}
+            <canvas className="w-100 full-height-canvas text-white z-0" ref={canvasRef} onClick={endGame}>Mango?</canvas>
         </div>
 
     )
