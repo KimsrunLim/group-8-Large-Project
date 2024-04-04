e0c5644e3b203137b86185e9248cc9b4c54435e7
Frosty204 <austinfrost04@gmail.com>
2024-04-03 23:05:17 -0400

    Started on Typing Game

--- gameon-frontend-web/src/components/SpeedTyping.js
+++ gameon-frontend-web/src/components/SpeedTyping.js
@@ -1,7 +1,35 @@
-import React from "react";
+import { useState } from "react";
 
 function SpeedTyping()
 {
+    const display = ["Display text"];
+    const [input, setInput] = useState('');
+    const [index, setIndex] = useState(0);
+    const [mistakes, setMistakes] = useState(0);
+    const [WPM, setWPM] = useState(0);
+    const [timer, setTimer] = useState(60);
+
+    const keyPress = (event) => {
+        setInput(event.input)
+        if (event.input === 'Backspace' && index > 0) {
+            --index;
+            display[index].classList.remove('correct', 'incorrect');
+        }
+        else {
+            if (event.key === display.charAt[index]) {
+                display[index].classList.add('correct');
+                display[index].classList.remove('incorrect');
+            }
+            else {
+                display[index].classList.add('incorrect');
+                display[index].classList.remove('correct');
+            }
+            if (index < display.length - 1) {
+                ++index;
+            }
+        }
+    }
+
     return (
         <p>typeRacer game</p>
     )
