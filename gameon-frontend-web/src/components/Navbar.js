61f828b9f3d16288752b5f711a86cc33f187fc1e
Yixin Zhao <st97334@gmail.com>
2024-04-04 15:10:54 -0400

    update navbar

--- gameon-frontend-web/src/components/Navbar.js
+++ gameon-frontend-web/src/components/Navbar.js
@@ -1,43 +1,54 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+import { faHouse, faUser } from '@fortawesome/free-solid-svg-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Navbar() {
     return (
         <>
-            <style>
-                {`
-                    .nav-link {
-                        color: #fff;
-                        font-weight: 500;
-                        font-size: 1.5rem;
-                    }
-                `}
-            </style>
-
-            <nav className="navbar navbar-expand-lg" style={{
-                backgroundColor: '#000', paddingLeft: '2%', paddingRight: '2%'
-            }}>
-                <a className="navbar-brand nav-link d-flex align-items-center" href="/">
-                    <span className="ps-1">Game<img src="/logo192.png" width="30" height="30"
-                        className="d-inline-block ps-1" alt=""/>n</span>
-                </a>
-                <div className="d-flex ms-auto">
-                    <a className="nav-item nav-link active me-5" href="/about">About</a>
-                    { /* functionality on leaderboard:
-                       * - logged in & IF user score exists: show user at top of stats  */ }
-                    <a className="nav-item nav-link active me-5" href="/leaderboard">Rank</a>
-                    <a className="nav-item nav-link" width="30" height="30" href="/profile">
-                        <FontAwesomeIcon icon={faUser} />
-                        { /* functionality on profile image click:
-                           * - if logged in -> account page
-                           * - else -> signup page  */ }
-                    </a>
+        
+            <nav className="navbar navbar-expand-lg navbar-dark bg-black">
+                {/* Logo */}
+                <div className="container-fluid fw-bold text-white"> 
+                    <div className="navbar-brand d-flex align-items-center me-auto ms-3">
+                        Game <img src="/logo192.png" width="22" height="22" className="d-inline-block" alt="Game On Logo"/>N!
+                    </div>
+                    <div className="d-flex align-items-center ms-auto">
+                        {/* Home */}
+                        <div className="nav-item">
+                            <a className="nav-link active ps-4 pe-4" href="/home">
+                                <FontAwesomeIcon icon={faHouse} /> {/* Add tooltip */}
+                            </a>
+                        </div>
+                        {/* About Us */}
+                        <div className="nav-item">
+                            <a className="nav-link active ps-4 pe-4" href="/about">ABOUT</a>
+                        </div>
+                        {/* Leaderboard/Rank */}
+                        <div className="nav-item">
+                            <a className="nav-link active ps-4 pe-4" href="/leaderboard">Rank</a>
+                        </div>
+                        {/* Profile Dropdown */}
+                        {/* look into menu forms & external content */}
+                        <div className="nav-item pe-4">
+                            <div className="btn-group">
+                                <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <FontAwesomeIcon icon={faUser} /> {/* Add tooltip */}
+                                </button>
+                                <div className="dropdown-menu dropdown-menu-right">
+                                    <span className="dropdown-item name">Welcome!</span>
+                                    <div className="dropdown-divider"></div>
+                                    <button className="dropdown-item edit-profile" onClick={"hi"}>Edit Profile</button>
+                                    <button className="dropdown-item signup/logout" onClick={"bye"}>Log Out</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </nav>
         </>
     );
-};
+}
 
 export default Navbar;
