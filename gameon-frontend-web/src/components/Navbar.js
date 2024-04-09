bd9b1a412a276ce09e1365ace89cba67e868397c
sol <soleilcordray@gmail.com>
2024-04-09 04:33:29 -0400

    nav learning

--- gameon-frontend-web/src/components/Navbar.js
+++ gameon-frontend-web/src/components/Navbar.js
@@ -1,64 +1,83 @@
-import React, {useState} from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faRankingStar, faUsers, faUser, faSignInAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Navbar, Nav, Dropdown } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHouse, faUsers, faRankingStar, faUser, faSignInAlt, faSignOutAlt
+} from '@fortawesome/free-solid-svg-icons';
 import Logo from '../assets/GameOnLogoWhite.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Header() {
-    const loggingOut = () => {
+    const [collapsible, setCollapsible] = useState(window.innerWidth <= 768); // "md"
+
+    const logOut = () => {
         document.cookie = "username= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-        window.location.href = "/";
-    }
+        window.location.href = "/login";
+    };
+
+    useEffect(() => {
+        const changeWidth = () => {
+            setCollapsible(window.innerWidth <= 768); // "md"
+        };
+
+        window.addEventListener('resize', changeWidth);
+
+        return () => {
+            window.removeEventListener('resize', changeWidth);
+        };
+    }, []);
+
+    const navItems = [
+        { name: 'Home', url: '/home', icon: faHouse },
+        { name: 'About', url: '/about', icon: faUsers },
+        { name: 'Ranks', url: '/leaderboard', icon: faRankingStar },
+        { name: 'User', icon: faUser, children: [
+            { name: 'Log In', url: '/login', icon: faSignInAlt },
+            { name: 'Log Out', action: logOut, icon: faSignOutAlt }
+        ]}
+    ];
+
+    const navItemClass = collapsible ? "ms-1 d-flex justify-content-center" : ""; // Custom class for centering nav items
+
+    const renderNavItems = (items) => {
+        return items.map((item, index) => (
+            item.url ? (
+                <Nav.Link key={index} as={Link} to={item.url} className={`text-white pe-3 m-1 ${navItemClass}`}>
+                    <FontAwesomeIcon icon={item.icon} /><span className="ps-2">{item.name}</span>
+                </Nav.Link>
+            ) : item.children ? (
+                <Dropdown key={index}>
+                    <Dropdown.Toggle as={Nav.Link} className={`text-white m-1 ${navItemClass}`}>
+                        <FontAwesomeIcon icon={item.icon} /><span className="ps-2">{item.name}</span>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        {item.children.map((child, childIndex) => (
+                            <Dropdown.Item key={childIndex} as={Link} to={child.url || '#'} onClick={child.action}>
+                                <FontAwesomeIcon icon={child.icon} /><span className="ps-2">{child.name}</span>
+                            </Dropdown.Item>
+                        ))}
+                    </Dropdown.Menu>
+                </Dropdown>
+            ) : null
+        ));
+    };
 
     return (
-        <>
-            <Navbar collapseOnSelect expand="md" className="bg-black" style={{zIndex: 1030}}>
-                <Container>
-                    <Navbar.Brand>
-                        <img src={Logo} style={{height: "2rem"}} alt="GameOn Logo"></img>
-                    </Navbar.Brand>
-
-                    {/* Automatically collapse navbar on selected screens */}
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" data-bs-theme="dark"/>
-
-                    {/* Navigation items that are collapsed on selected screens */}
-                    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-center">
-                        <Nav>
-                            <Nav.Link className="text-white pe-3 m-1" href="/home">
-                                <FontAwesomeIcon icon={faHouse} /><span className="ps-2">Home</span>
-                            </Nav.Link>
-                            
-                            <Nav.Link className="text-white pe-3 m-1" href="/about">
-                                <FontAwesomeIcon icon={faUsers} /><span className="ps-2">About</span>
-                            </Nav.Link>
-
-                            <Nav.Link className="text-white pe-3 m-1" href="/leaderboard">
-                                <FontAwesomeIcon icon={faRankingStar} /><span className="ps-2">Ranks</span>
-                            </Nav.Link>
-
-                            <Dropdown>
-                                <Dropdown.Toggle as={Nav.Item} className="text-white m-1">
-                                    <FontAwesomeIcon icon={faUser} /><span className="ps-2">User</span>
-                                </Dropdown.Toggle>
-
-                                <Dropdown.Menu>
-                                    <Dropdown.Item href="/login">
-                                        <FontAwesomeIcon icon={faSignInAlt} />
-                                        <span className="ps-2">Log In</span>
-                                    </Dropdown.Item>
-                                    <Dropdown.Item onClick={loggingOut}>
-                                        <FontAwesomeIcon icon={faSignOutAlt} />
-                                        <span className="ps-2">Log Out</span>
-                                    </Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
+        <Navbar collapseOnSelect expand="md" className="bg-black" style={{ zIndex: 1030 }}>
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    <img src={Logo} style={{ height: "2rem" }} alt="GameOn Logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" data-bs-theme="dark"/>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className={collapsible ? "flex-column" : "flex-row"}>
+                        {renderNavItems(navItems)}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
-};
+}
 
 export default Header;
