b60f5097411ee31ababbb210a4e2143be4113cf3
sol <soleilcordray@gmail.com>
2024-04-10 05:15:54 -0400

    sol_new stylized login

--- gameon-frontend-web/src/components/Login.js
+++ gameon-frontend-web/src/components/Login.js
@@ -4,7 +4,9 @@ import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
 import Logo from '../assets/Logo-Black.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+//
+// Functionality
+//
 
 function Login() {
     var username;
@@ -74,14 +76,24 @@ function Login() {
         return;
     };
 
+    //
+    // Styling
+    //
+
+    const linkHover = "link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover";
+
+    const [isGuestHovered, setIsGuestHovered] = useState(false);
+    const [isSubmitHovered, setIsSubmitHovered] = useState(false);
+
     return (
         <>
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-md-6">
+                        
                         { /* Logo */}
-                        <div className="logo p-4 text-center m-auto">
-                            <img src={Logo} className='img-fluid' alt="GameOn Logo" style={{ maxWidth: "150px" }} />
+                        <div className="logo pt-5 py-4 d-flex justify-content-center">
+                            <img src={Logo} className='img-fluid' alt="GameOn Logo" style={{ maxWidth: "28rem" }} />
                         </div>
 
                         <div className="login-container">
@@ -89,56 +101,109 @@ function Login() {
                             { /* Login Box */}
                             <div className="card">
 
-                                { /* Title */}
-                                <h1 className="card-header p-3 text-center align-middle fw-bold text-black">Log In</h1>
+                                { /* Header: Title */}
+                                <h1 className="card-header p-3 text-center align-middle fw-bold 
+                                        text-white border-bottom border-5 border-primary bg-black">
+                                    Log In
+                                </h1>
+                                
+                                { /* Form */}
                                 <div className="card-body">
-
-                                    { /* Form */}
                                     <form onSubmit={doLogin}>
 
                                         { /* Username */}
-                                        <div className="form-group pt-4">
-                                            <label className="pb-1 text-secondary fw-bold">Username</label>
-                                            <div className="input-group mb-3">
-                                                <span className="input-group-text"><FontAwesomeIcon icon={faUser} /></span>
-                                                <input type="text" id="username" data-testid="username-input" className="form-control" placeholder="Username" ref={(c) => username = c} />
+                                        <div className="form-group py-3">
+                                            <label className="pb-2 text-dark fw-bold fs-5">
+                                                Username
+                                            </label>
+                                            <div className="input-group border rounded">
+                                                <span className="input-group-text px-3 border-0 border-end">
+                                                    <FontAwesomeIcon icon={faUser} />
+                                                </span>
+                                                <input className="form-control form-control-lg
+                                                    border-0 border-end rounded bg-transparent"
+                                                    type="text" id="username" data-testid="username-input" 
+                                                    placeholder="Username" ref={(c) => username = c} />
                                             </div>
                                         </div>
 
                                         { /* Password */}
-                                        <div className="form-group pt-1">
-                                            <label className="pb-1 text-secondary fw-bold">Password</label>
-                                            <div className="input-group mb-3">
-                                                <span className="input-group-text"><FontAwesomeIcon icon={faLock} /></span>
-                                                <input type="password" id="password" data-testid="password-input" className="form-control" placeholder="Password" ref={(c) => password = c} />
+                                        <div className="form-group py-3">
+                                            <label className="pb-2 text-dark fw-bold fs-5">
+                                                Password
+                                            </label>
+                                            <div className="input-group border rounded">
+                                                <span className="input-group-text px-3 border-0 border-end">
+                                                    <FontAwesomeIcon icon={faLock} />
+                                                </span>
+                                                <input className="form-control form-control-lg 
+                                                    border-0 border-end rounded bg-transparent"
+                                                    type="password" id="password" data-testid="password-input" 
+                                                    placeholder="Password" ref={(c) => password = c} />
                                             </div>
                                         </div>
+
                                         { /* Error Feedback */}
                                         { /* idea: red container highlight on error message... flash/until type */}
                                         {message &&
-                                            <div data-testid="error-message" className="text-danger mt-1 mb-2 pt-2 pb-2 text-center mx-auto fs-6 fw-bold">
+                                            <div data-testid="error-message" className="text-danger 
+                                                mt-1 mb-2 pt-2 pb-2 text-center mx-auto fs-6 fw-bold">
                                                 {message}
                                             </div>
                                         }
 
                                         { /* Submit */}
-                                        <button type="submit" data-testid="submit-button" className="btn bg-black w-100 mt-3 mb-1 fs-5 text-white fw-bold">Submit</button>
+                                        <button type="submit" data-testid="submit-button" 
+                                            className="btn mt-4 w-100 fs-5 fw-bold"
+                                            onMouseEnter={() => setIsSubmitHovered(true)}
+                                            onMouseLeave={() => setIsSubmitHovered(false)}
+                                            style={{
+                                                backgroundColor: isSubmitHovered ? 'black' : '#007bff',
+                                                color: isSubmitHovered ? 'white' : 'black', 
+                                            }}
+                                        >
+                                            Submit
+                                        </button>
                                     </form>
 
-                                    <div className='d-flex justify-content-center align-items-center text-center p-2 fw-medium'>
-                                        <a href="/forgotpassword" className='link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover ps-3'>Forgot Password?</a>
+                                    {/* Forgot Password */}
+                                    <div className="d-flex justify-content-center align-items-center 
+                                        text-center py3 fs-6 fw-medium text-secondary link-secondary">
+                                        <a href="/forgotpassword" className={`ps-3 link-secondary ${linkHover}`}>
+                                            Forgot Password?
+                                        </a>
                                     </div>
 
-                                    <div className='d-flex justify-content-center align-items-center text-center p-2 fw-medium'>
+                                    { /* Switch to Signup */}
+                                    <div className='footer py-3 border-top border-2 border-dark bg-light 
+                                            d-flex justify-content-center align-items-center text-center 
+                                            fw-medium rounded-bottom fs-6'>
                                         New to GameOn?
-                                        <a href="/signup" className='link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover ps-3'>Sign Up</a>
+                                        <a href="/signup" className={`fw-bold ps-3 ${linkHover}`}>Sign Up  
+                                        </a>
                                     </div>
                                 </div>
                             </div>
 
-                            { /* Switch to Signup */}
+                            { /* Continue as Guest */}
                             <div className='d-grid py-5'>
-                                <button onClick={handleGuest} className="btn border border-3 border-dark fw-bold" type="button" style={{ maxHeight: "50px", height: "50px" }}>Continue as Guest</button>
+                                <button onClick={handleGuest} type="button" 
+                                    className="btn"
+                                    onMouseEnter={() => setIsGuestHovered(true)}
+                                    onMouseLeave={() => setIsGuestHovered(false)}
+                                    style={{ 
+                                        maxHeight: "50px", 
+                                        height: "50px",
+                                        borderWidth: '3px',
+                                        backgroundColor: isGuestHovered ? 'black' : '#F8F9FA',
+                                        borderColor: isGuestHovered ? '#007bff' : 'black',
+                                        color: isGuestHovered ? 'white' : 'black',
+                                    }
+                                }>
+                                    <h5 className="fw-bold pt-1">
+                                        Continue as Guest
+                                    </h5>
+                                </button>
                             </div>
                         </div>
                     </div>
