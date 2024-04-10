1a9a94bc4d2a507f8164f4e397af3ad09a9f2727
KimsrunLim <ksrunlim@gmail.com>
2024-04-10 15:27:03 -0400

    Added description

--- gameon-frontend-web/src/components/Home.js
+++ gameon-frontend-web/src/components/Home.js
@@ -8,23 +8,25 @@ import typingPreview from "../assets/game1.gif";
 import "../pages/SpeedTypingPage"
 import Carousel from 'react-bootstrap/Carousel';
 
-function Home()
-{
+function Home() {
+
   return (
-    <Carousel indicators={false} data-bs-theme="dark">
+    <Carousel indicators={false} data-bs-theme="dark" className='pt-5'>
       <Carousel.Item withinterval={5000}>
-          <div className="text-center">
-            <img  className='mb-4' src={typingPreview} alt='#'/>
-            <h2 className="text-black mb-4">Typing Speed Test</h2>
-            <a href= {"speedtyping"} className="btn btn-info" tabindex="-1" role="button">Play</a>
-          </div>
+        <div className="text-center">
+          <img className='mb-4 justify-content-center img-fluid' src={typingPreview} alt='#' />
+          <h2 className="text-black mb-4">Measure how quickly and accurately</h2>
+          <h2 className="text-black mb-4">you can type text within a minute!</h2>
+          <a href={"speedtyping"} className="btn btn-primary btn-lg w-50" tabindex="-1" role="button">Play</a>
+        </div>
       </Carousel.Item>
       <Carousel.Item interval={5000}>
-          <div className="text-center">
-            <img className='mb-4' src={reactionPreview} alt='#'/>
-            <h2 className="text-black text-center mb-4">Reaction Speed Test</h2>
-            <a href= {"reactiongame"} className="btn btn-info" tabindex="-1" role="button">Play</a>
-          </div>
+        <div className="text-center">
+          <img className='mb-4 justify-content-center img-fluid' src={reactionPreview} alt='#' />
+          <h2 className="text-black text-center mb-4">Measure the speed at which</h2>
+          <h2 className="text-black text-center mb-4">you can react to a specific cue!</h2>
+          <a href={"reactiongame"} className="btn btn-primary btn-lg w-50" tabindex="-1" role="button">Play</a>
+        </div>
       </Carousel.Item>
     </Carousel>
   );
