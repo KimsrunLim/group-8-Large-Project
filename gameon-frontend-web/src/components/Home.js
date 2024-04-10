f48a13463bc04e0c20479d3fc40da4214b770fe2
sol <soleilcordray@gmail.com>
2024-04-10 05:16:03 -0400

    Remove unused variables

--- gameon-frontend-web/src/components/Home.js
+++ gameon-frontend-web/src/components/Home.js
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import temp1 from "../assets/astronaut.png";
-import temp2 from "../assets/celebrating.png";
+// import temp1 from "../assets/.png";
+// import temp2 from "../assets/.png";
 // import temp3 from "../assets/taken.png";
 import "../pages/SpeedTypingPage"
 
@@ -14,13 +14,13 @@ import Carousel from 'react-bootstrap/Carousel';
 const gameView = [
   {
     name: 'Type Racer',
-    img: temp1,
+    // img: temp1,
     overview: 'type as fast as you can',
     link: "speedtyping"
   },
   {
     name: 'Reaction Game',
-    img: temp2,
+    // img: temp2,
     overview: 'click when the screen color change',
     link: "reactiongame"
   },
@@ -50,7 +50,7 @@ function Home()
     <Carousel indicators={false} data-bs-theme="dark">
       <Carousel.Item withinterval={5000}>
           <div className="text-center">
-            <img src={temp1} alt='#' className="bg-black"/>
+            {/* <img src={ temp1} alt='#' className="bg-black"/> */}
             <h2 className="text-black">Type Racer</h2>
             <p className="text-black">type as fast as you can</p>
             <a href= {"speedtyping"} className="btn btn-info" tabindex="-1" role="button">Play</a>
@@ -58,7 +58,7 @@ function Home()
       </Carousel.Item>
       <Carousel.Item interval={5000}>
           <div className="text-center">
-            <img src={temp2} alt='#'/>
+            {/* <img src={temp2} alt='#'/> */}
             <h2 className="text-black text-center">Reaction Game</h2>
             <p className="text-black text-center">click when the screen color change</p>
             <a href= {"reactiongame"} className="btn btn-info" tabindex="-1" role="button">Play</a>
